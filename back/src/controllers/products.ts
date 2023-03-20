a6722ec98dcf2254a3e46999ae529ca7762b2af5
Alvaro Garzón <alvaro.develops@gmail.com>
2023-03-20 18:24:48 +0100

    delete product ok

--- back/src/controllers/products.ts
+++ back/src/controllers/products.ts
@@ -1,15 +1,19 @@
 /* eslint-disable @typescript-eslint/restrict-template-expressions */
+import fs from 'fs'
 import { IProduct } from './../types.d'
 import toDataBase from '../utils/validationData'
 import { Request, Response } from 'express'
-import products from '../db/products.json'
+
+const data = fs.readFileSync('./src/db/products.json')
+const parsedData: IProduct[] = JSON.parse(data as unknown as string)
+console.log(parsedData)
 
 export const getProducts = (_req: Request, res: Response): Response<IProduct[]> => {
-  return res.status(200).json({ products })
+  return res.status(200).json({ products: parsedData })
 }
 
 export const getProductById = (req: Request, res: Response): Response<IProduct | string> => {
-  const productById = products.filter((product: IProduct) => product.id === +req.params.id)
+  const productById = parsedData.filter((product: IProduct) => product.id === +req.params.id)
   try {
     if (productById !== null) {
       return res.status(200).json({ product: productById })
@@ -21,15 +25,19 @@ export const getProductById = (req: Request, res: Response): Response<IProduct |
 }
 
 export const postProduct = (req: Request, res: Response): any => {
-  const newId = products[products.length - 1].id + 1
+  const newId = parsedData[parsedData.length - 1].id + 1
   const newProduct = {
     id: newId,
     ...req.body
   }
   try {
     const validatedProduct = toDataBase(newProduct)
-    console.log(validatedProduct)
-    products.push(validatedProduct)
+    parsedData.push(validatedProduct)
+    fs.writeFile('./src/db/products.json', JSON.stringify(parsedData), err => {
+      if (err != null) {
+        throw err
+      }
+    })
     return res.status(201).json(validatedProduct)
   } catch (error) {
     return res.status(500).json(`Product not added: ${error}`)
@@ -37,12 +45,27 @@ export const postProduct = (req: Request, res: Response): any => {
 }
 
 export const putProduct = (req: Request, res: Response): any => {
-  let productById = products.filter((product: IProduct) => product.id === req.body.id)
+  let productById = parsedData.filter((product: IProduct) => product.id === req.body.id)
   try {
     productById = req.body
     const validatedProduct = toDataBase(productById)
     return res.status(201).json(validatedProduct)
   } catch (error) {
     return res.status(500).json(`Product not added: ${error}`)
+  }
+}
+
+export const deleteProduct = (req: Request, res: Response): Response => {
+  const newData = parsedData.filter((product: IProduct) => product.id !== +req.params.id)
+  console.log(newData)
+  try {
+    fs.writeFile('./src/db/products.json', JSON.stringify(newData), err => {
+      if (err != null) {
+        throw err
+      }
+    })
+    return res.status(200).json('Product deleted')
+  } catch (error) {
+    return res.status(500).json(`Product not deleted: ${error}`)
   }
 }
--- back/src/routes/products.ts
+++ back/src/routes/products.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { getProducts, getProductById, postProduct, putProduct } from '../controllers/products'
+import { getProducts, getProductById, postProduct, putProduct, deleteProduct } from '../controllers/products'
 
 const productsRouter = express.Router()
 
@@ -9,6 +9,7 @@ productsRouter.get('/:id', getProductById)
 
 productsRouter.post('/', postProduct)
 productsRouter.put('/:id', putProduct)
+productsRouter.delete('/:id', deleteProduct)
 
 // productsRouter.put('/:id', (_req,res) => {
 //   res.send(productsData)
