f74676bf2537b92a3e8bb5627839380dca5d98ed
Alvaro Garzón <alvaro.develops@gmail.com>
2023-03-21 22:27:12 +0100

    fetch function w. cors

---
+++ front/src/services/api.tsx
@@ -0,0 +1,18 @@
+// import { type IProduct } from '../interfaces'
+
+export const fetchProducts = async (): Promise<any> => {
+  const backURL = import.meta.env.VITE_API_URL
+  try {
+    const resp = await fetch(backURL, {
+      mode: "cors",
+      headers: {
+        'Access-Control-Allow-Origin': 'http://localhost:5173/'
+      }
+    });
+    const data = await resp.json();
+    return data;
+  } catch (error) {
+    return error;
+  }
+}
+
