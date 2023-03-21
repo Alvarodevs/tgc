fbda75995c7c8aa748a491b219ec4b9c3e1dae29
Alvaro Garzón <alvaro.develops@gmail.com>
2023-03-21 22:32:15 +0100

    index.ts added w.cors

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors from 'cors'
 import * as dotenv from 'dotenv'
 import productsRouter from './routes/products'
 
@@ -7,6 +8,7 @@ dotenv.config()
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 const PORT = process.env.PORT ?? 3001
 
