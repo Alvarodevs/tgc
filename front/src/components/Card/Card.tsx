3b204a343c12ee1fd95b149b23c2e9b29413f0a0
Alvaro Garzón <alvaro.develops@gmail.com>
2023-03-21 13:45:01 +0100

    feat: product as prop

--- front/src/components/Card/Card.tsx
+++ front/src/components/Card/Card.tsx
@@ -7,38 +7,38 @@ import {
   Rate,
   Brand,
   Description,
-  Price
+  Price,
 } from "./CardStyle"
 import { Link } from "react-router-dom"
-import { AiFillStar } from 'react-icons/ai'
+import { AiFillStar } from "react-icons/ai"
+import { type IProduct } from "../../interfaces"
 
-const Card = (): JSX.Element => {
-  const mockInfo = {
-    id: 0,
-    title: "Iphone 46",
-    description: "Lorem ipsum dolor",
-    price: 10,
-    discountPercentage: 20.0,
-    rating: 4,
-    stock: 40,
-    brand: "Brand",
-    category: "Shoes",
-    thumbnail: "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
-    images: [],
-  }
-
+const Card = ({
+  id,
+  title,
+  description,
+  price,
+  discountPercentage,
+  rating,
+  stock,
+  brand,
+  category,
+  thumbnail,
+  images,
+}: IProduct): JSX.Element => {
   return (
     <Container>
       <Link to={`/product/1`}>
         <CardItem>
-          <Title>{mockInfo.title}</Title>
-          <AiFillStar className='rateIcon'>
-          </AiFillStar>
-            <Rate>{mockInfo.rating}</Rate>
-          <Image src={mockInfo.thumbnail} />
-          <Brand>{mockInfo.brand} <span>presents: </span></Brand>
-          <Description>{mockInfo.description}</Description>
-          <Price>€{mockInfo.price}</Price>
+          <Title>{title}</Title>
+          <AiFillStar className="rateIcon"></AiFillStar>
+          <Rate>{rating}</Rate>
+          <Image src={thumbnail} />
+          <Brand>
+            {brand} <span>presents: </span>
+          </Brand>
+          <Description>{description}</Description>
+          <Price>€{price}</Price>
 
           <Button>Edit</Button>
         </CardItem>
--- front/src/views/Home/Home.tsx
+++ front/src/views/Home/Home.tsx
@@ -2,9 +2,24 @@ import Card from '../../components/Card'
 import { Container } from './HomeStyle'
 
 const Home = (): JSX.Element => {
+
+  const mockInfo = {
+    id: 0,
+    title: "Iphone 46",
+    description: "Lorem ipsum dolor",
+    price: 10,
+    discountPercentage: 20.0,
+    rating: 4,
+    stock: 40,
+    brand: "Brand",
+    category: "Shoes",
+    thumbnail: "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
+    images: [],
+  }
+
   return (
     <Container>
-      <Card/>
+      <Card {...mockInfo}/>
     </Container>
   )
 }
